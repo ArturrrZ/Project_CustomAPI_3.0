d7eb4eb5d92a835da81b68eb7742b83d702c6e1a
Artur Z <arturziianbaev@gmail.com>
2023-10-25 22:06:10 -0700

    random facts about me

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, jsonify
-
+import random
 data= {
     'f_name':{'first_name': 'Artur',},
     'l_name':{'last_name': 'Ziianbaev',},
     'age':{'current_age': '22',},
     'date_of_birth': {"day":"23","month":"January","year":"2001","_full_date":"01/23/2001"}
 }
+
+FACTS_ABOUT_ME=[
+    "I do not have any tattoos",
+    "I tried alcohol at the age of 12 for the first time (quite by chance xD; Don't think that I am an alcoholic person)",
+    "I used to play soccer for 15 years",
+    "I've been going to the gym for 6 years",
+    "I have a younger brother. He is 10 years old",
+    "I've read 5 books during the previous year",
+    "I lived in Ohio for 18 months"
+]
 
 app=Flask(__name__)
 
@@ -36,6 +46,17 @@ def show_age():
 @app.route("/personal_info")
 def show_full_info():
     return jsonify(data),200
+
+#random facts
+@app.route("/random_fact")
+def show_random_fact():
+    random_fact=random.choice(FACTS_ABOUT_ME)
+    response={
+        "random_fact": random_fact
+    }
+    return response
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
